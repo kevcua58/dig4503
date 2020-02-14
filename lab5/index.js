87f727b1c1c897d91a6261a5a220ca28f8b523eb
Kevin Cua <kevcua58@knights.ucf.edu>
2020-02-14 18:22:20 -0500

    lab5 finished

---
+++ lab5/index.js
@@ -0,0 +1,55 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+const chalk = require("chalk");
+
+const pokemons = require("json-pokemon");
+const pokeDex = pokemons;
+
+// App.use(Express.json());
+App.use("/", Express.static("public"));
+
+App.get("/id/:id", (req, res) => {
+    let result = {"error" : "Sorry, Pokemon doesn't exist!"};
+
+    pokeDex.forEach((value) => {
+        if(value.id == req.params.id) {
+            // console.log(req.path);
+            result = value;
+        }
+    });
+
+    if(result.error) {
+        console.log(chalk.red(req.path));
+    }
+    else {
+        console.log(chalk.green(req.path));
+    }
+
+    res.json(result);
+});
+
+App.get("/name/:name", (req, res) => {
+    let result = {"error" : "Sorry, Pokemon doesn't exist!"};
+
+    pokeDex.forEach((value) => {
+        if(value.name.toLowerCase() == req.params.name) {
+            // console.log(req.path);
+            result = value;
+        }
+    });
+
+    if(result.error) {
+        console.log(chalk.red(req.path));
+    }
+    else {
+        console.log(chalk.green(req.path));
+    }
+
+    res.json(result);
+});
+
+App.listen(port, function(){
+    console.log("Server running!");
+});
